fc4dee7737c11e10484977fa4da2fd5a59be710e
William Fortson <40750425+WillFJS@users.noreply.github.com>
2023-07-25 00:23:34 -0500

    Update constants.js

--- src/constants.js
+++ src/constants.js
@@ -1,2 +1,2 @@
-export const CHIEF_WS_ENDPOINT = ___WS_ENDPOINT___ || 'wss://chief.placenl.nl/ws';
+export const CHIEF_WS_ENDPOINT = ___WS_ENDPOINT___ || 'wss://placestart.up.railway.app/ws';
 export const USERSCRIPT_REVISION = ___GIT_HASH___ || 'development';
